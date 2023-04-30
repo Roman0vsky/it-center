90027465950c081a02f2cd5cff14754d3f73b7eb
mikan48 <mukanshin.san@gmail.com>
2023-04-30 22:36:44 +0300

    Updated few pages (without testing)

---
+++ ITCenterBack/ITCenterBack/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using ITCenterBack.Interfaces;
+using ITCenterBack.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITCenterBack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        private readonly ICourseService _courseService;
+        private readonly IMapper _mapper;
+
+        public HomeController(ICourseService courseService, IMapper mapper)
+        {
+            _courseService = courseService;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            var courses = await _courseService.GetAllCoursesAsync();
+            var coursesVM = _mapper.Map<List<CourseViewModel>>(courses);
+
+            var page = new IndexViewModel
+            {
+                Header = new HeaderViewModel
+                {
+                    Courses = coursesVM
+                }
+            };
+
+            return View();
+        }
+    }
+}
---
+++ ITCenterBack/ITCenterBack/ViewModels/IndexViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ITCenterBack.ViewModels
+{
+    public class IndexViewModel
+    {
+        public HeaderViewModel Header { get; set; }
+    }
+}
