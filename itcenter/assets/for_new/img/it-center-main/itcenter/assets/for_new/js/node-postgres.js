799b1b4fd074332af059c0ce8cf9188461a60aa3
Anastasia Borisenko <borisenko.nastusha@gmail.com>
2023-05-02 23:25:17 +0300

    gg

---
+++ itcenter/assets/for_new/img/it-center-main/itcenter/assets/for_new/js/node-postgres.js
@@ -0,0 +1,16 @@
+document.write("dsff")
+
+import PG from 'pg';
+
+const Pool = PG.Pool;
+
+document.write("33333dsff")
+//export const connection  =  new Pool({
+ //   user: 'odoo',
+ //   host: 'localhost',
+ ////   database: 'itcenter',
+  //  password: 'odoo',
+ //   port: 5432,
+//  })
+  //client.connect()
+  document.write("dsff")
